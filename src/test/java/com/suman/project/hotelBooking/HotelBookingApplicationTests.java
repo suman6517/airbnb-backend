233a84c378289b25a32f0257542148dd4fa77a17
Suman Baral <ssumanbaral55@gmail.com>
2026-01-01 01:41:54 +0530

    Initial commit

---
+++ src/test/java/com/suman/project/hotelBooking/HotelBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suman.project.hotelBooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
